2908d73c73b9f06bcb70eb94b36d61901c836580
maria <ma.carrero@duocuc.cl>
2026-06-30 08:00:49 -0400

    actualizar vitr config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',   // asegura rutas relativas en producción
   server: {
     port: 5174,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:8080', // solo se usa en desarrollo
         changeOrigin: true,
         rewrite: (path) => path
       }
